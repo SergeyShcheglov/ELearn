bd0a6452ae4afe8a1e3ff6f02cf0a20d89a7c785
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-22 17:35:14 +0600

    first commit

---
+++ ELearn/ELearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELearnApp.swift
+//  ELearn
+//
+//  Created by Sergey Shcheglov on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ELearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
