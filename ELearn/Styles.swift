3d61833cdb6d3645783458767e5eb52d2d396a08
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-26 23:08:45 +0600

    Add modifier and extenstion for strokeStyle

---
+++ ELearn/Styles.swift
@@ -0,0 +1,37 @@
+//
+//  Styles.swift
+//  ELearn
+//
+//  Created by Sergey Shcheglov on 26.04.2022.
+//
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
+    var cornerRadius: CGFloat
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                    .stroke(
+                        .linearGradient(
+                            colors: [
+                                .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                                .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                            ],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                    .blendMode(.overlay)
+            }
+    }
+}
+
+extension View {
+    func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
+    }
+}
+
