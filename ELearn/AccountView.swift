12aa5969163894b710f06b40237429ef45e6d68b
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-29 22:55:12 +0600

    Add swipe actions, restructured code

--- ELearn/AccountView.swift
+++ ELearn/AccountView.swift
@@ -8,86 +8,121 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var isDeleted = false
+    @State private var isPinned = false
+    
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                        )
-                    
-                    Text("Sergey Shcheglov")
-                        .font(.title.weight(.semibold))
-                    
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.large)
-                        
-                        Text("Russia")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
+                profile
                 
-                Section {
-                    NavigationLink {
-                        ContentView()
-                    } label: {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink {
-                        
-                    } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    
-                    NavigationLink {
-                        
-                    } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.green)
-                .listRowSeparator(.hidden)
+                menu
                 
-                Section {
-                    Link(destination: URL(string: "https://apple.com")!) {
-                        HStack {
-                            Label("Website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("YouTube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparator(.hidden)
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+                )
+            
+            Text("Sergey Shcheglov")
+                .font(.title.weight(.semibold))
+            
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.large)
+                
+                Text("Russia")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink {
+                ContentView()
+            } label: {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink {
+                
+            } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            
+            NavigationLink {
+                
+            } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.green)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://apple.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                    Button(action: { isDeleted = true } ) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    
+                    pinButton
+                }
+            }
+            
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("YouTube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var pinButton: some View {
+        Button { isPinned.toggle() } label: {
+            Label(isPinned ? "Unpin" : "Pin", systemImage: isPinned ? "pin.slash" : "pin")
+        }
+        .tint(isPinned ? .gray : .yellow)
     }
 }
 
