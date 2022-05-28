4caf728e46acccb0423a06e1acd736f659a7977d
Sergey Shcheglov <97shcheglov@gmail.com>
2022-05-28 18:55:20 +0600

    customized a lil bit

---
+++ ELearn/Tab.swift
@@ -0,0 +1,20 @@
+//
+//  Tab.swift
+//  ELearn
+//
+//  Created by Sergey Shcheglov on 28.05.2022.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tab_Previews: PreviewProvider {
+    static var previews: some View {
+        Tab()
+    }
+}
