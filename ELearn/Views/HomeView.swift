5e48f51e07eca44d57a2470c7fd8523194be5152
Sergey Shcheglov <97shcheglov@gmail.com>
2022-08-11 13:08:00 +0300

    add features, clean

--- ELearn/Views/HomeView.swift
+++ ELearn/Views/HomeView.swift
@@ -10,30 +10,53 @@ import SwiftUI
 struct HomeView: View {
     @State private var hasScrolled = false
     var body: some View {
-        ScrollView {
-            GeometryReader { proxy in
-                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-            }
-            .frame(height: 0)
+        ZStack {
+            Color("Background").ignoresSafeArea()
             
-            FeaturedItem()
-        }
-        .coordinateSpace(name: "scroll")
-        .onPreferenceChange(ScrollPreferenceKey.self) { value in
-            withAnimation(.easeInOut) {
-                if value < 0 {
-                    hasScrolled = true
-                } else {
-                    hasScrolled = false
+            ScrollView {
+                scrollDetection
+                
+                featured
+            }
+            .coordinateSpace(name: "scroll")
+            .onPreferenceChange(ScrollPreferenceKey.self) { value in
+                withAnimation(.easeInOut) {
+                    if value < 0 {
+                        hasScrolled = true
+                    } else {
+                        hasScrolled = false
+                    }
                 }
             }
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 70)
+            }
+            .overlay (
+                NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
+                    .opacity(hasScrolled ? 1 : 0)
+            )
+            
         }
-        .safeAreaInset(edge: .top) {
-            Color.clear.frame(height: 70)
+    }
+    
+    var scrollDetection: some View {
+        GeometryReader { proxy in
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
         }
-        .overlay (
-            NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
-                .opacity(hasScrolled ? 1 : 0)
+        .frame(height: 0)
+    }
+    
+    var featured: some View {
+        TabView {
+            ForEach(courses) { item in
+                FeaturedItem(course: item)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
         )
     }
 }
